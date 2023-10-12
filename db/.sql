9c545fbdf7c5016d410a92bf17db8495d7e48d31
MauxC <72012637+MauxC@users.noreply.github.com>
2023-10-12 19:24:30 +0200

    Db (#15)
    * Final merge before Delivery (#7)
    
    * [ADD] auth route template
    
    * [ADD] middleware check JWT
    
    * [ADD] middleware check JWT
    
    * [ADD] DB connection, User class & auth routes with db communication
    
    * [UPD] register route tested with insomnia, connection to db with env
    
    * [ADD] login for admin + sql table
    
    * [ADD] - Workflow to test if the project builds on Merge Request
    
    * [ADD] - Workflow to test if the MR comes from dev / test if it builds / (COMMENTED) run Jest tests.
    
    * [ADD] - Example of environment variables needed to run the project
    
    ---------
    
    Co-authored-by: MauxC <camille.maux@epitech.eu>
    Co-authored-by: MauxC <72012637+MauxC@users.noreply.github.com>
    
    * Revert "Final merge before Delivery (#7)" (#12)
    
    This reverts commit 581fa1b2eed5691f8a5a0d8a5c0b4b218709e524.
    
    * Final push for delivery (#11)
    
    * [ADD] auth route template
    
    * [ADD] middleware check JWT
    
    * [ADD] middleware check JWT
    
    * [ADD] DB connection, User class & auth routes with db communication
    
    * [UPD] register route tested with insomnia, connection to db with env
    
    * [ADD] login for admin + sql table
    
    * [ADD] - Workflow to test if the project builds on Merge Request
    
    * [ADD] - Workflow to test if the MR comes from dev / test if it builds / (COMMENTED) run Jest tests.
    
    * [ADD] - Example of environment variables needed to run the project
    
    * Google auth (#8)
    
    * [ADD] google register base
    
    * [ADD] strategies for google register  & login
    
    * [ADD] google auth with ID token - couldn't test because front not done
    
    * [MOD] google register
    
    * [UP] - Correct stupid inequality error
    
    ---------
    
    Co-authored-by: MauxC <camille.maux@epitech.eu>
    Co-authored-by: MauxC <72012637+MauxC@users.noreply.github.com>
    
    * [UPD] updated the sql scheme to the new archtechture
    
    * UPD: user type file
    
    * [UPD] updated login & register routes for new user type
    
    * [REM] remove password salt from db
    
    * [REM] remove password salt from db
    
    * [REM] remove password salt from db
    
    * [ADD] Swagger login & register description
    
    * [ADD] winston & morgan logging
    
    * [ADD] winston rotation for log files
    
    * [MOD] lock.json
    
    ---------
    
    Co-authored-by: ancadeau <57462280+ancadeau@users.noreply.github.com>

--- db/.sql
+++ db/.sql
@@ -2,25 +2,147 @@ CREATE DATABASE IF NOT EXISTS `wandercore` DEFAULT CHARACTER SET utf8 COLLATE ut
 
 USE `wandercore`;
 
-CREATE TABLE users (
+CREATE TABLE user_account (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255),
-    connected_with VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE societies (
+CREATE TABLE external_provider (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    ws_end_point VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO external_provider (name, ws_end_point) VALUES ('google', 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=');
+
+CREATE TABLE user_login_data_external (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    user_id INT NOT NULL,
+    external_provider_id INT NOT NULL,
+    external_provider_token VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id),
+    FOREIGN KEY (external_provider_id) REFERENCES external_provider(id)
+);
+
+CREATE TABLE user_login_data (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    user_id INT NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id)
+);
+
+CREATE TABLE society (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE admins (
+CREATE TABLE location (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
     society_id INT NOT NULL,
+    name VARCHAR(250) UNIQUE NOT NULL,
+    map_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (society_id) REFERENCES societies(id)
+    FOREIGN KEY (society_id) REFERENCES society(id)
+);
+
+CREATE TABLE user_role (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    user_id INT NOT NULL,
+    location_id INT NOT NULL,
+    rank INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    color VARCHAR(7) NOT NULL,
+    location_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE map (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    location_id INT NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    width INT NOT NULL,
+    height INT NOT NULL,
+    floors INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+ALTER TABLE location ADD COLUMN map_id INT NOT NULL;
+ALTER TABLE location ADD FOREIGN KEY (map_id) REFERENCES map(id);
+
+CREATE TABLE map_tile (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    map_id INT NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL,
+    floor INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (map_id) REFERENCES map(id)
+);
+
+CREATE TABLE room (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    location_id INT NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    map_tile_id INT NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL,
+    geometry TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (map_tile_id) REFERENCES map_tile(id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE room_rank (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    room_id INT NOT NULL,
+    rank INT NOT NULL,
+    rule INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES room(id)
+);
+
+CREATE TABLE type (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    icon_path VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE pin_point (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    room_id INT NOT NULL,
+    x INT NOT NULL,
+    y INT NOT NULL,
+    destination BOOLEAN NOT NULL DEFAULT FALSE,
+    type_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES room(id),
+    FOREIGN KEY (category_id) REFERENCES category(id),
+    FOREIGN KEY (type_id) REFERENCES type(id)
+);
+
+CREATE TABLE favorite (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    user_id INT NOT NULL,
+    pin_point_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id),
+    FOREIGN KEY (pin_point_id) REFERENCES pin_point(id)
 );
