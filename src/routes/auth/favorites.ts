5e3a676b4edef8cc424c4997fe57408e9597d5b4
ancadeau <antoine.cadeau@epitech.eu>
2023-10-21 21:33:34 +0900

    [UP] Added the add and delete routes

--- src/routes/auth/favorites.ts
+++ src/routes/auth/favorites.ts
@@ -22,4 +22,46 @@ FavoriteRouter.get("/", async (req, res) => {
   }
 });
 
+FavoriteRouter.post("/", async (req, res) => {
+  const userId = req.body.decoded.id;
+  const locationId = req.body.locationId;
+  const pinPointId = req.body.pinPointId;
+
+  if (!userId || !locationId)
+    return res.status(400).json({ message: "Invalid request" });
+
+  try {
+    const rows = await db.query(
+      "INSERT INTO user_favorites (user_id, location_id, pin_point_id) VALUES (?, ?, ?)",
+      [userId, locationId, pinPointId]
+    );
+
+    return res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+FavoriteRouter.delete("/", async (req, res) => {
+  const userId = req.body.decoded.id;
+  const locationId = req.body.locationId;
+  const pinPointId = req.body.pinPointId;
+
+  if (!userId || !locationId)
+    return res.status(400).json({ message: "Invalid request" });
+
+  try {
+    const rows = await db.query(
+      "DELETE FROM user_favorites WHERE user_id = ? AND location_id = ? AND pin_point_id = ?",
+      [userId, locationId, pinPointId]
+    );
+
+    return res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default FavoriteRouter;
