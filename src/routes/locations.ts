6e32e5613bb809d7e13e24151c27cbac39a2e6c8
MauxC <camille.maux@epitech.eu>
2023-11-20 17:06:35 +0100

    [ADD] location swagger documentation

--- src/routes/locations.ts
+++ src/routes/locations.ts
@@ -1,3 +1,168 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Locations
+ *  description: Locations managing API
+ * /societies/{id}/locations:
+ *  get:
+ *   summary: Get all locations of a society
+ *   tags: [Locations]
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: integer
+ *       required: true
+ *       description: The society ID
+ *   responses:
+ *    200:
+ *     description: The list of locations
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items:
+ *         type: object
+ *         properties:
+ *          id:
+ *            type: integer
+ *            description: The location ID
+ *          name:
+ *           type: string
+ *           description: The location name
+ *          society_id:
+ *           type: integer
+ *           description: The society ID
+ *    500:
+ *     description: Internal server error
+ * 
+ *  post:
+ *    summary: Create a new location
+ *    tags: [Locations]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *         type: integer
+ *         required: true
+ *         description: The society ID
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema: 
+ *        type: object
+ *        required:
+ *         - name
+ *        properties:
+ *         name:
+ *          type: string
+ *          description: The location name
+ *    responses:
+ *     200:
+ *      description: The location was successfully created
+ *     400:
+ *      description: Invalid request
+ *     500:
+ *      description: Internal server error
+ * 
+ * /societies/{id}/locations/{locId}:
+ *   get:
+ *    summary: Get a location by ID
+ *    tags: [Locations]
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The society ID
+ *     - in: path
+ *       name: locId
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The location ID
+ *    responses:
+ *     200:
+ *      description: The location description by ID
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          id:
+ *           type: integer
+ *           description: The location ID
+ *          name:
+ *           type: string
+ *           description: The location name
+ *          society_id:
+ *           type: integer
+ *           description: The society ID
+ *     500:
+ *      description: Internal server error
+ * 
+ *   delete:
+ *    summary: Delete a location by ID
+ *    tags: [Locations]
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The society ID
+ *     - in: path
+ *       name: locId
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The location ID
+ *    responses:
+ *     200:
+ *      description: The location was successfully deleted
+ *     500:
+ *      description: Internal server error
+ * 
+ *   put:
+ *    summary: Update a location by ID
+ *    tags: [Locations]
+ *    parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The society ID
+ *     - in: path
+ *       name: locId
+ *       schema:
+ *        type: integer
+ *        required: true
+ *        description: The location ID
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema: 
+ *        type: object
+ *        required:
+ *         - name
+ *        properties:
+ *         name:
+ *          type: string
+ *          description: The location name
+ *    responses:
+ *     200:
+ *      description: The location was successfully updated
+ *     400:
+ *      description: Invalid request
+ *     500:
+ *      description: Internal server error
+ * 
+ */
+
 import { Router } from "express";
 import db from "../database/mariadb";
 import logger from "../utils/logger";
